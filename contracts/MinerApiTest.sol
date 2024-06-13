1cbfa04f195d61ed333a82d05f4097a30620999e
KimDaeikk <dylan@zetacube.net>
2024-06-13 17:23:36 +0900

    hardhat init

---
+++ contracts/MinerApiTest.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {FilAddress} from "fevmate/contracts/utils/FilAddress.sol";
+import {MinerAPI, MinerTypes, CommonTypes} from "filecoin-solidity-api/contracts/v0.8/MinerAPI.sol";
+
+contract MinerApiTest {
+    using FilAddress for address;
+
+    error API_ERROR(int256);
+
+    function getOwner(uint64 _minerId) public returns (bytes memory) {
+        CommonTypes.FilActorId minerId = CommonTypes.FilActorId.wrap(_minerId);
+        (int256 exitcode, MinerTypes.GetOwnerReturn memory ownerReturn) = MinerAPI.getOwner(minerId);
+        if (exitcode != 0) revert API_ERROR(exitcode);
+        return ownerReturn.owner.data;
+    }
+
+    // function changeOwnerAddress() public returns (string) {
+        
+    // }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,40 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+if (!PRIVATE_KEY) {
+    throw new Error("Please set your PRIVATE_KEY in a .env file");
+}
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.17",
+    defaultNetwork: "calibrationnet",
+    networks: {
+        localnet: {
+            chainId: 31415926,
+            url: "http://127.0.0.1:1234/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
+        calibrationnet: {
+            chainId: 314159,
+            url: "https://api.calibration.node.glif.io/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
+        filecoinmainnet: {
+            chainId: 314,
+            url: "https://api.node.glif.io",
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+};
+
+export default config;
